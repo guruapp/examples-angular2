f8ad1f27d38ebb6c094ebf459394c0b7322bc438
Vadim L <netvideo@gmail.com>
2016-08-10 20:51:07 +0300

    ng-init parent index

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,4 +1,12 @@
 app = angular.module("myApp",[])
     .controller("myCtrl", function ($scope) {
-       
+       $scope.countries = [
+           {
+               name:"India",
+               cities:[
+                   {name:"Heberabad"},
+                   {name:"Chenai"}
+               ]
+           }
+       ]
     })
