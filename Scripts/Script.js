8c5d09fc2093ad2edca24b448a39610d27483608
Vadim L <netvideo@gmail.com>
2016-08-11 20:57:44 +0300

    ng-include - file in html

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,12 +1,11 @@
 app = angular.module("myApp",[])
     .controller("myCtrl", function ($scope) {
-       $scope.countries = [
-           {
-               name:"India",
-               cities:[
-                   {name:"Heberabad"},
-                   {name:"Chenai"}
-               ]
-           }
-       ]
+        $scope.employees = [
+            {name:"Vadim",gender: "male", salary:5500},
+            {name:"Sara",gender: "female", salary:6800},
+            {name:"Mark",gender: "male", salary:5700},
+            {name:"Pam",gender: "female", salary:5300},
+            {name:"Todd",gender: "male", salary:5200}
+        ]
+
     })
